d29de9654f5d72323c5e377e3e0080522c6dc6ce
prishsha <prishavadhavkar@gmail.com>
2026-02-07 10:55:08 +0530

    basic app running

--- server/AlertManager.java
+++ server/AlertManager.java
@@ -1,7 +1,7 @@
 public class AlertManager {
     public static void check(String log) {
         if (log.contains("\"level\":\"ERROR\"")) {
-            System.out.println("🚨 ALERT DETECTED: " + log);
+            System.out.println("ALERT DETECTED: " + log);
         }
     }
 }
--- server/DashboardHandler.java
+++ server/DashboardHandler.java
@@ -6,8 +6,14 @@ public class DashboardHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+
         StringBuilder html = new StringBuilder();
-        html.append("<html><body><h1>Central Log Dashboard</h1>");
+
+        html.append("<html><head>");
+        html.append("<meta http-equiv='refresh' content='2'>");
+        html.append("</head><body>");
+
+        html.append("<h1>Central Log Dashboard</h1>");
 
         for (String log : LogHandler.logs) {
             html.append("<p>").append(log).append("</p>");
@@ -15,8 +21,9 @@ public class DashboardHandler implements HttpHandler {
 
         html.append("</body></html>");
 
-        exchange.sendResponseHeaders(200, html.length());
-        exchange.getResponseBody().write(html.toString().getBytes());
+        byte[] response = html.toString().getBytes();
+        exchange.sendResponseHeaders(200, response.length);
+        exchange.getResponseBody().write(response);
         exchange.close();
     }
 }
